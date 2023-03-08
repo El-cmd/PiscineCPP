ae4573fb7a2c0af002762138733f1887460c9a35
Eldoctor <lothvalentin@gmail.com>
2023-03-08 18:09:36 +0100

    08/03

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -1,12 +1,46 @@
 #include "Bureaucrat.hpp"
 
-Bureaucrat::Bureaucrat()
+Bureaucrat::Bureaucrat(): name("Doc"), grade(1)
 {
     std::cout << "Default Bureaucrat constructor called\n";
 }
 
+Bureaucrat::Bureaucrat(std::string n, int g): name(n), grade(g)
+{
+    std::cout << "Surcharge constructor called\n";
+}
 Bureaucrat::~Bureaucrat()
 {
-    stsd::cout << "Default Bureaucrat destructor called\n";
+    std::cout << "Default Bureaucrat destructor called\n";
 }
 
+int Bureaucrat::getGrade()
+{
+    return (this->grade);
+}
+
+std::string Bureaucrat::getName()
+{
+    return(this->name);
+}
+
+const char *Bureaucrat::GradeTooHighException()
+{
+    return ("Grade is too high\n");
+}
+const char *Bureaucrat::GradeTooLowException()
+{
+    return ("Grade is too low\n");
+}
+
+void Bureaucrat::down()
+{
+    if (this->grade > 150)
+    throw ;
+    this->grade++;
+}
+
+void Bureaucrat::up()
+{
+    this->grade--;
+}
--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -3,13 +3,24 @@
 
 #include <string>
 #include <iostream>
+#include <exception>
 
 class Bureaucrat
 {
     public:
     Bureaucrat();
+    Bureaucrat(std::string n, int g);
     ~Bureaucrat();
+    const char *GradeTooHighException();
+    const char *GradeTooLowException();
+    void down();
+    void up();
+    int getGrade();
+    std::string getName();
+    
     private:
+    std::string const name;
+    int grade;
 
 };
 
