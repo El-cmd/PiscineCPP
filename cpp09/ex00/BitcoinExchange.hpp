e08f8aabc5ba0aa729cecbbc24bc1732a690e3e0
Valentin LOTH <vloth@paul-f4Ar1s2.clusters.42paris.fr>
2024-03-24 06:48:33 +0100

    base ex00

--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -5,8 +5,26 @@
 #include <exception>
 #include <string>
 #include <iostream>
+#include <map>
 
 
+class BitcoinConverter
+{
+	public:
+	// Coplien
+	BitcoinConverter(std::string input);
+	BitcoinConverter(const BitcoinConverter &other);
+	BitcoinConverter &operator=(const BitcoinConverter &other);
+
+	//getter
+
+	//Member Functions
+
+	private:
+	BitcoinConverter();
+	std::map<std::string, float> data;
+	std::map<std::string, float> input;
+};
 
 
 #endif
